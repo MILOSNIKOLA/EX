ed3dc491d5a0295332f45dd817f1aae1bfba1e0e
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2025-09-01 19:48:53 +0200

    Add files via upload

---
+++ 1 From Scratch TP EX/EX/4. Javascript/Le DOM/supports/cours/index.js
@@ -0,0 +1,33 @@
+// SELECTEURS 
+
+
+// document.querySelector("h4").style.background = "red";
+
+// const baliseHTML = document.querySelector("h4");
+
+// console.log(baliseHTML);
+
+// baliseHTML.style.color = "green";
+
+
+// Click event
+
+
+const questionContainer = document.querySelector(".click-event");
+
+// console.log(questionContainer);
+
+questionContainer.computedStyleMap.border
+
+
+
+
+
+
+
+
+
+
+
+
+
