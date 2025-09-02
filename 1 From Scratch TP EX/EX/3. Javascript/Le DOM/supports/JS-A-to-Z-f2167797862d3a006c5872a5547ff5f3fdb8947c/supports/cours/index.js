884e319defc82edd39bb00063cd288bd59890282
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2025-09-02 20:51:19 +0200

    Add files via upload

---
+++ 1 From Scratch TP EX/EX/3. Javascript/Le DOM/supports/JS-A-to-Z-f2167797862d3a006c5872a5547ff5f3fdb8947c/supports/cours/index.js
@@ -0,0 +1,59 @@
+// SELECTEURS 
+
+
+// document.querySelector("h4").style.background = "red";
+
+// const baliseHTML = document.querySelector("h4");
+
+// console.log(baliseHTML);
+
+// baliseHTML.style.color = "green";
+
+
+// Click event
+
+
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+// console.log(btn1, btn2);
+
+
+questionContainer.addEventListener("click", () => {
+
+questionContainer.classList.toggle("question-clicked");
+
+})
+
+btn1.addEventListener("click", () => {
+    response.classList.add("show-response");
+    response.style.background = "green";
+    questionContainer.style.background = "#096919ff";
+    // response.style.visibility = "visible";
+    
+})
+
+btn2.addEventListener("click", () => {
+    response.classList.add("show-response");
+   
+    response.style.background = "red";
+    questionContainer.style.background = "#690909ff";
+    // response.style.visibility = "visible";
+})
+
+// Mouse event 
+
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+   mousemove.style.left = e.pageX + "px";
+   mousemove.style.top = e.pageY + "px";
+});
+
+
+
+
+
+
