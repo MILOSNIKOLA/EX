13ccd6dfafcafcd52fe5f6c3fc6164bc45b4f6d2
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2025-09-08 20:52:34 +0200

    Add files via upload

--- 1 From Scratch TP EX/EX/3. Javascript/Le DOM/[TP1 présentation] Mouse effect/index.js
+++ 1 From Scratch TP EX/EX/3. Javascript/Le DOM/[TP1 présentation] Mouse effect/index.js
@@ -5,3 +5,11 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+// mix-blend-mode: difference;
+const difference = document.querySelector(".body");
+
+window.addEventListener("mouseup", (e) => {
+  difference.style.mixBlendMode = "overlay";
+//   difference.style.transform = "scale(1) translate(-50%, -50%)";
+});
