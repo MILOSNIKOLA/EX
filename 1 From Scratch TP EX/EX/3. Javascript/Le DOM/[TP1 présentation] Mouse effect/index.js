54a7fca935a5987a74d5355b63346aef055d0a1d
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2025-09-09 16:20:36 +0200

    Add files via upload

--- 1 From Scratch TP EX/EX/3. Javascript/Le DOM/[TP1 présentation] Mouse effect/index.js
+++ 1 From Scratch TP EX/EX/3. Javascript/Le DOM/[TP1 présentation] Mouse effect/index.js
@@ -7,9 +7,44 @@
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
 
 // mix-blend-mode: difference;
-const difference = document.querySelector(".body");
 
-window.addEventListener("mouseup", (e) => {
-  difference.style.mixBlendMode = "overlay";
-//   difference.style.transform = "scale(1) translate(-50%, -50%)";
-});
+const circle = document.querySelector(".circle");
+// const circle1 = document.querySelector(".circle-1");
+// const circle2 = document.querySelector(".circle-2");
+// const circle3 = document.querySelector(".circle-3");
+// const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  // circle1.style.left = e.pageX + "px";
+  // circle1.style.top = e.pageY + "px";
+  // circle2.style.left = e.pageX + "px";
+  // circle2.style.top = e.pageY + "px";
+  // circle3.style.left = e.pageX + "px";
+  // circle3.style.top = e.pageY + "px";
+  console.log(e.circle1);
+  
+})
+
+const mousemove = document.querySelector(".mousemove");
+const circle1 = document.querySelector(".circle-1");
+const circle2 = document.querySelector(".circle-2");
+const circle3 = document.querySelector(".circle-3");
+
+window.addEventListener("mousemove", (e) => {
+   mousemove.style.left = e.pageX + "px";
+   mousemove.style.top = e.pageY + "px";
+   console.log(e);
+   
+  circle.style.transform = "translate(-50%, -50%)";
+  circle1.style.transform = "translate(-50%, -50%)";
+  circle2.style.transform = "translate(-50%, -50%)";
+  circle3.style.transform = "translate(-50%, -50%)";
+  circle1.style.left = e.pageX + "px";
+  circle1.style.top = e.pageY + "px";
+  circle2.style.left = e.pageX + "px";
+  circle2.style.top = e.pageY + "px";
+  circle3.style.left = e.pageX + "px";
+  circle3.style.top = e.pageY + "px";
+  circle.style.left = e.pageX + "px";
+  circle.style.top = e.pageY + "px";
+  });
