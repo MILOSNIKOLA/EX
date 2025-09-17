8b611f4711a12f8e7957ff6b8c209cd8882edd44
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2025-09-17 21:47:35 +0200

    Add files via upload

--- 1 From Scratch TP EX/EX/3. Javascript/Le DOM/[TP3 présentation] Scroll Popup/script.js
+++ 1 From Scratch TP EX/EX/3. Javascript/Le DOM/[TP3 présentation] Scroll Popup/script.js
@@ -7,3 +7,47 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+
+/************** NAVBAR **************/
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+   
+    if (window.scrollY > 0) {
+        nav.style.height = "50px";
+    } else {
+        nav.style.height = "100px";
+    }
+})
+
+/**************** IMAGE **************/
+
+const image = document.getElementById('imgImporovise');
+
+window.addEventListener('scroll', () =>  {
+    if (window.scrollX > 500) {
+        image.style.opacity = 1;
+        image.style.transform = "translateX(0)";
+    } else {
+        image.style.opacity = 0;
+        image.style.transform = "translateX(-200px)";
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
