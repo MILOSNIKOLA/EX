b341fc87d323022447a3aeac55b568ea3410c32a
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2025-09-18 18:49:24 +0200

    Add files via upload

---
+++ 1 From Scratch TP EX/EX/3. Javascript/Le DOM/[TP3 présentation] Scroll Popup/index.js
@@ -0,0 +1,43 @@
+// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+
+// S'assurer que les liens sont clickables
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+// : difference;
+
+const mousemove = document.querySelector(".mousemove");
+const circle = document.querySelector(".circle");
+const circle1 = document.querySelector(".circle-1");
+const circle2 = document.querySelector(".circle-2");
+const circle3 = document.querySelector(".circle-3");
+
+
+// window.addEventListener("mousemove", (e) => {
+//   circle1.classList.toggle("hand");
+//   });
+
+window.addEventListener("mousemove", (e) => {
+  circle.style.left = e.pageX + "px";
+  circle.style.top = e.pageY + "px";
+  circle1.style.left = e.pageX + "px";
+  circle1.style.top = e.pageY + "px";
+  circle2.style.left = e.pageX + "px";
+  circle2.style.top = e.pageY + "px";
+  circle3.style.left = e.pageX + "px";
+  circle3.style.top = e.pageY + "px";
+  console.log(mousemove);
+  
+});
+   
+// document.querySelectorAll('p, h3,li').forEach(element => {
+//   element.style.cursor = 'pointer';
+//   element.classList.add("hand");
+// });
+
+
+
+
+  
