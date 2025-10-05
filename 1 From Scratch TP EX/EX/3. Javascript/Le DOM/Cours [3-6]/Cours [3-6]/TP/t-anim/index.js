a584f75cdb46bf1a3084b7d629e2e464ed737b11
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2025-10-05 20:13:08 +0200

    Add files via upload

--- 1 From Scratch TP EX/EX/3. Javascript/Le DOM/Cours [3-6]/Cours [3-6]/TP/t-anim/index.js
+++ 1 From Scratch TP EX/EX/3. Javascript/Le DOM/Cours [3-6]/Cours [3-6]/TP/t-anim/index.js
@@ -1,1 +1,50 @@
-alert("Hello, World!");
+//  Anime.js
+
+const target = document.getElementById('target');
+let array = ["développeur", "intégrateur", "freelance"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+const createLetter = () => {
+    const letter = document.createElement('span');
+    target.appendChild(letter);
+
+    letter.textContent = array[wordIndex][letterIndex];
+
+    setTimeout(() => {
+        letter.remove();
+    },2800);
+}
+
+// createLetter();
+
+// setInterval > setTimeout , qui peut s'appeler elle meme
+
+// setInterval(() => {
+//     letterIndex++;
+//     createLetter();
+// }, 200)
+
+const loop = () => {
+    setTimeout(() => {
+        if(wordIndex >= array.length) {
+            wordIndex = 0;
+        }
+        if (letterIndex < array[wordIndex].length) {
+            createLetter();
+            letterIndex++;
+            loop();
+        } else {
+            wordIndex++;
+            letterIndex = 0;
+            setTimeout(() => {
+                loop();
+            }, 2800);
+           
+        }
+    }, 60)
+}
+
+
+loop();
+
