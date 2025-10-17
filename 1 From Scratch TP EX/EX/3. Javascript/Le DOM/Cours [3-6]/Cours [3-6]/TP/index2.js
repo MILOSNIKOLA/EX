2511d6812909ad8ac6133e6bcf20225c805d98d8
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2025-10-17 15:35:36 +0200

    Add files via upload

--- 1 From Scratch TP EX/EX/3. Javascript/Le DOM/Cours [3-6]/Cours [3-6]/TP/index2.js
+++ 1 From Scratch TP EX/EX/3. Javascript/Le DOM/Cours [3-6]/Cours [3-6]/TP/index2.js
@@ -1,2 +1,39 @@
 
 // Methode Objet
+
+
+let data = [
+    {
+    nom: "Mika",
+    age: 25,
+    technologies: ["JavaScript", "React", "Node.js"],
+    admin: false
+    },
+    {
+    nom: "Nikola",
+    age: 31,
+    technologies: ["PHP", "React", "Typescript"],
+    admin: true
+    },
+    {
+    nom: "Dragan",
+    age: 37,
+    technologies: ["JavaScript", "HTML", "CSS"],
+    admin: true
+    }
+];
+
+document.body.innerHTML = data
+// .filter((user) => user.admin)
+.filter((user) => user.nom.includes("a"))
+.sort((a, b) => b.age - a.age)
+.map((user) => 
+`
+<div class="user-card">
+    <h3>${user.nom}</h3>
+    <p>Age: ${user.age} ans</p>
+    <p>Admin: ${user.admin ? "Oui" : "Non"}</p>
+</div>
+`
+).join("");
+
